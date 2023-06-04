9d40f943d7c3bb3d0315e4fd3e5401f303fb8f68
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2023-06-04 19:15:49 +0100

    testing

--- src/App.js
+++ src/App.js
@@ -3,23 +3,46 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <header>
+        <div className="menu">
+          <ul>
+            <li>
+              <a>Por quién votar</a>
+            </li>
+            <li>
+              <a href="#">Nosotros</a>
+            </li>
+          </ul>
+        </div>
+        </header>
+
+
+        <main>
+
+        <div className="App">
+          <p>Aprende qué está detrás de tu voto</p>
+        </div>
+
+        <h1>Candidatos</h1>
+
+        <div className="Candidatos">
+          <ul>
+            <li>
+              <a href="https://es.wikipedia.org/wiki/Aaron_Swartz" target="_blank" rel="noopener noreferrer">
+                Jon Topic
+              </a>
+            </li>
+            <li>Yaku Perez</li>
+            <li>Leonidas Iza</li>
+            <li>Fernando Villaviciencio</li>
+          </ul>
+        </div>
+        </main>
+
     </div>
   );
 }
+
 
 export default App;
