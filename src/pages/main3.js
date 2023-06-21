5c395074846a085115056b26a7491ad61ce54993
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2023-06-21 03:56:22 +0100

    add

--- src/pages/main3.js
+++ src/pages/main3.js
@@ -26,18 +26,126 @@ function OttoSonnenholzner() {
 
       <div className="Propuestas">
         <ul>
-          <li className="propuestas-1">Seguridad</li>
-          <li className="propuestas-2">Bitcoin</li>
-          <li className="propuestas-3">Economía</li>
-          <li className="propuestas-4">
-            <a href="#bitcoin"> <span style={{ color: 'black' }}>Salud</span></a>
+        <ul>
+          <li className="propuestas-1">
+          <a href="#Seguridad"> <span style={{ color: 'black' }}> Seguridad🦺</span></a> 
+            </li>
+         <li className="propuestas-2">
+         <a href="#Instituciones"> <span style={{ color: 'black' }}>Instituciones</span></a> 
+          
           </li>
+        <li className="propuestas-3">Económico</li>
+          <li className="propuestas-4">
+            Social
+          </li>
+          <li className="propuestas-5">Libre de Drogas</li>
+          <li className="propuestas-6">Educación</li>
+          <li className="propuestas-7">Calidad de vida</li>
+          <li className="propuestas-8">Vivienda</li>
+          <li className="propuestas-9">Politicas de Género</li>
+          <li className="propuestas-10">Politica exterior</li>
+          <li className="propuestas-11">Migración Ecuatoriana</li>
+          <li className="propuestas-12">Transición Ecológica</li>
+          <li className="propuestas-13">Bienestar Animal</li>
+          <li className="propuestas-14">Empleo</li>
+          <li className="propuestas-15">Estimulo a la Inversión</li>
+          <li className="propuestas-16">Liquidez</li>
+          <li className="propuestas-17">Crecimiento Económico</li>
+          <li className="propuestas-18">Inversiones</li>
+          <li className="propuestas-19">Emprendimiento</li>
+          <li className="propuestas-20">Agricultura </li>
+          <li className="propuestas-21">Empresa</li>
+          <li className="propuestas-22">Reforma Tributarias</li>
+          <li className="propuestas-23">Turismo</li>
+          <li className="propuestas-24">Fortalecimiento Institucional</li>
+          <li className="propuestas-25">Conectividad</li>
+          <li className="propuestas-26">Corrupción</li>
+          <li className="propuestas-27">Legislación</li>
+         
+        </ul>
         </ul>
       </div>
 
       <footer>
-        <section id="bitcoin">
-          Hola, ¿cómo estás? Estamos hablando de Bitcoin ahora y queremos que esto se solucione muy bien para un futuro descentralizado.
+      <section id="Seguridad">
+        1. Protección de los derechos y libertades:<p></p> Sin seguridad, los ciudadanos no son 
+libres para ejercer sus derechos de forma plena, afectando su calidad de vida y 
+participación cívica, vital para el desarrollo democrático.<p></p>
+
+2. Preservación del orden público:<p></p> El estado debe mantener y fortalecer el orden para 
+prevenir el caos y la violencia en todas sus formas, eso incluye proteger la vida e 
+integridad de los ciudadanos ante todo tipo de amenazas como la delincuencia, el 
+crimen organizado, el terrorismo y otras formas de violencia sistemática. <p></p>
+
+3. Mantenimiento del orden público:<p></p> Sin cohesión social no existe confianza en el 
+Estado y por tanto tampoco en sus instituciones. Buscamos crear un entorno en el 
+que las personas se sientan seguras y protegidas para poder vivir en armonía. <p></p>
+
+ 
+4.Desarrollo económico: <p></p>La economía también se cimenta sobre la confianza y el 
+desarrollo, sobre la seguridad. Los ciudadanos deben sentirse libres y seguros de 
+abrir un negocio, de comerciar con tranquilidad. A mayor escala, si el Estado no 
+garantiza la seguridad, esto también genera efectos en la Inversión Extranjera 
+puesto que pone en peligro a la cadena de producción de cualquier inversionista. <p>
+
+5.Defender los derechos humanos: <p></p>El Estado debe proteger los derechos humanos 
+de los ciudadanos, incluyendo el derecho a la vida, integridad personal, libertad y 
+seguridad.</p>
+        </section>
+        <section id="Instituciones">
+
+Mantendremos  este  estrecho  contacto  además  entre  las  funciones  del  Estado, 
+promoviendo la creación de un Consejo de Estado en el cual se integren, no sólo de 
+manera emergente, sino de manera permanente las cabezas de cada uno de los poderes 
+del  Estado,  para  mantener  activas  decisiones  y  seguimiento  que  atañen  a  todos  en 
+conjunto como estado, por encima de las funciones específicas de cada uno.  
+ 
+En este sentido el Palacio de Carondelet, ya no será más el palacio de reuniones del 
+presidente y sus ministros, sino el centro del ejercicio del seguimiento continuo de la 
+responsabilidad del Estado, por parte de todos sus poderes.  
+ 
+Las reuniones del Consejo de Estado serán objeto del libre acceso por parte de toda la 
+ciudadanía a través de los mecanismos de gobierno electrónico. Las únicas reuniones del 
+Consejo de Estado que tendrán carácter de reservadas serán aquellas que hagan relación 
+a estrategias y cumplimiento de planes en materia de seguridad y lucha contra el crimen 
+organizado.  
+ 
+Introduciremos  reformas  en  las  cuales  parea  suprimir  las  ternas  completas  para 
+funcionarios de designación en manos del poder ejecutivo, y se reemplazarán por ternas  
+ 
+ 
+conformadas en conjunto por todos los poderes del Estado, a partir de mecanismos de 
+participación ciudadana abiertos.  
+ 
+Promoveremos perfiles de mayor nivel e idoneidad para ocupar los más altos cargos de 
+representación de cada entidad de control, promoviendo además la obligatoriedad de que 
+los puestos de decisión institucionales sólo puedan ser ocupados por servidores de carrera, 
+de alto desempeño y reconocida honradez, de cada una de esas entidades de control.  
+ 
+Pero más allá de la promoción de reformas legales o eventuales reformas constitucionales 
+necesarias para acceder a estos objetivos, propondremos una campaña de sinceramiento 
+y transparencia de la actual condición de los procesos de control que, sólo reflejan una 
+estadística muerta, en la que se esconde la impunidad de servidores públicos que por 
+omisión de acciones de control oportunas y eficaces, han dejado de ser, en el tiempo, 
+verdaderos sujetos de control y fiscalización.  
+ 
+Las reformas propuestas están encaminadas a eliminar los trámites caducados represados 
+en el sistema de control que, sólo sirve en la práctica para entorpecer el despacho de las 
+causas activas y sobre las cuales aún caben acciones concurrentes o “ex post” dentro del 
+sistema de control estatal de la Contraloría General del Estado.  
+ 
+El  proceso  de  transparentar,  sobre  todas  las  notificaciones  de  predeterminación  de 
+responsabilidades civiles culposas y administrativas y órdenes de reintegro sobre las que 
+hubieren  transcurrido  más  de  siete  años  previstos  legalmente  y  que  no  hayan  sido 
+notificadas con la respectiva resolución permitirá a la Contraloría General del Estado 
+incrementar la velocidad de procesos en las causas más recientes y que constituyen el 
+porcentaje más alto de perjuicios ocasionados al Estado que, se mantienen incobrables 
+sólo a causa del número de causas represadas y sin despacho.
+
+
+
+
+
         </section>
       </footer>
     </div>
